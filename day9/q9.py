044f7901db04881c200a09fa0e05ef347cd202ef
zobac <mikezobal@gmail.com>
2016-12-10 11:23:16 -0800

    Finished day 9
    Figured out the recursive solution.  I had to catch the 'maximum
    recursion' exception case...

--- day9/q9.py
+++ day9/q9.py
@@ -15,62 +15,61 @@ def question1():
     decompressedLength = 0
 
     with open('q9Input.txt', 'r') as f:
-	# no need to read each line individually
-	# create an iterator of the data so we can track where we are
-	# as we read through it
-	inputData = iter(''.join([l.strip() for l in f.readlines()]))
+        # no need to read each line individually
+        # create an iterator of the data so we can track where we are
+        # as we read through it
+        inputData = iter(''.join([l.strip() for l in f.readlines()]))
 
     try:
         while True:
 
-	    # read until we hit a '(' char
+            # read until we hit a '(' char
             decompressedLength += len(list(itertools.takewhile(notStart, inputData)))
 
-	    # extract the marker and the marker data
+            # extract the marker and the marker data
             marker = ''.join(itertools.takewhile(notEnd, inputData))
-	    (sub, repeat) = getMarkerInputs(marker)
+            (sub, repeat) = getMarkerInputs(marker)
 
-	    # activate the marker
-	    decompressedLength += len(list(itertools.islice(inputData, sub))) * repeat
+            # activate the marker
+            decompressedLength += len(list(itertools.islice(inputData, sub))) * repeat
 
     except ValueError:
-	# reached end of iterator
+        # reached end of iterator
         pass
 
     return decompressedLength
 
 
-def question2():
+def question2(inputData):
 
     decompressedLength = 0
 
-    with open('q9Input.txt', 'r') as f:
-	# no need to read each line individually
-	# create an iterator of the data so we can track where we are
-	# as we read through it
-	inputData = iter(''.join([l.strip() for l in f.readlines()]))
 
     try:
         while True:
 
-	    # read until we hit a '(' char
+            # read until we hit a '(' char
             decompressedLength += len(list(itertools.takewhile(notStart, inputData)))
 
-	    # extract the marker and the marker data
+            # extract the marker and the marker data
             marker = ''.join(itertools.takewhile(notEnd, inputData))
-	    (sub, repeat) = getMarkerInputs(marker)
+            (sub, repeat) = getMarkerInputs(marker)
 
-	    # activate the marker
-	    decompressedLength += len(list(itertools.islice(inputData, sub))) * repeat
+            # activate the marker recursively
+            decompressedLength += question2(itertools.islice(inputData, sub)) * repeat
 
     except ValueError:
-	# reached end of iterator
-        pass
-
-    return decompressedLength
+        # reached end of iterator or maximum recursion, time unstack
+        return decompressedLength
 
 
 if __name__ == "__main__":
 
+    with open('q9Input.txt', 'r') as f:
+    # no need to read each line individually
+    # create an iterator of the data so we can track where we are
+    # as we read through it
+        inputData = iter(''.join([l.strip() for l in f.readlines()]))
+
     print(question1())
-    print(question2())
+    print(question2(inputData))
